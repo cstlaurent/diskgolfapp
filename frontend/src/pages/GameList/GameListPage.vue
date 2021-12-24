b0289c1412c074fca58e7efd7bc6584735b8ae7d
mikeron <themike7171@gmail.com>
2021-12-23 19:24:17 -0500

    Auto stash before merge of "main" and "origin/main"

--- frontend/src/pages/GameList/GameListPage.vue
+++ frontend/src/pages/GameList/GameListPage.vue
@@ -3,50 +3,66 @@
 <script setup>
 import { ref } from 'vue'
 import Gamemodule from '../../components/Games.vue'
-let gamecount = 0
+import { nanoid } from 'nanoid'
+
 const games = ref([])
 const newPlayer = ref('')
 const newCourse = ref('')
 const newDate = ref('')
-const lignCount=0
+const isEditMode = ref(false)
+const editedGameid = ref('')
 
 function addGame() {
+  if (isEditMode.value === false) {
+    games.value.push({
+      id: nanoid(),
+      player: newPlayer.value,
+      course: newCourse.value,
+      date: newDate.value,
+    })
+  } else {
+    const g = games.value[editedGameid.value]
 
-  games.value.push({
-    id: gamecount++,
-    player: newPlayer.value,
-    course: newCourse.value,
-    date: newDate.value,
-})
-
-}
-
-function deleteGame(id){
-  games.value.splice(id,1)
-gamecount--
-
+    const newGame = {
+      id: g.id,
+      player: newPlayer.value,
+      course: newCourse.value,
+      date: newDate.value,
+    }
+    games.value.splice(id, 1)
   }
 
-  
+  newPlayer.value = ''
+  newCourse.value = ''
+  newDate.value = ''
+}
 
+function deleteGame(id) {
+  games.value.splice(id, 1)
+}
 
-
-
-
+function editGame(id) {
+  const gameToEdit = games.value[id]
+  isEditMode.value = true
+  newPlayer.value = gameToEdit.player
+  newCourse.value = gameToEdit.course
+  newDate.value = gameToEdit.date
+  editedGameid.value = id
+}
 </script>
 
 <template>
   <Gamemodule />
   <div>{{ games }}</div>
- 
- <!-- =======================================================
+
+  <!-- =======================================================
  BUTTON TEST POUR DELETE -->
- 
- <!-- <div>
+
+  <!-- <div>
 <button class="bg-red-400 rounded-md" @click="deleteGame"> DELETE TEST</button>
 
   </div> -->
-<!-- ========================================================== -->
+  <!-- ========================================================== -->
   <div class="border-2 border-blue-500 shadow-md rounded-3xl p-5 mt-56">
     <form @submit.prevent="addGame">
       <div class="mt-10 border-2">
@@ -69,6 +85,7 @@ gamecount--
       />
 
       <button
+        v-if="isEditMode === false"
         class="
           bg-blue-500
           hover:bg-blue-700
@@ -83,37 +100,70 @@ gamecount--
       >
         Add Game
       </button>
+
+      <button
+        v-else
+        class="
+          bg-blue-500
+          hover:bg-blue-700
+          text-white
+          font-bold
+          py-2
+          px-20
+          rounded-full
+          ml-60
+          mb-5
+        "
+      >
+        Save
+      </button>
     </form>
   </div>
 
-  <div class="mt-28 ml- ml-32 ">
-    <table class="border-2 border-blue-500 bg-blue-100     ">
-      <tr class=" font-bold text-xl  ">
-      
-       <td class="inline-block  text-center w-52 ">Players</td>
-        <td class="inline-block  text-center w-52 ">Course</td>
-        <td class="inline-block  text-center w-52  ">Date</td>
-        <td class="inline-block  text-center w-52  ">Id</td>
+  <table class="border-2 border-blue-500 bg-blue-100">
+    <tr class="font-bold text-xl">
+      <th>Players</th>
+      <th>Course</th>
+      <th>Date</th>
+      <th>Id</th>
 
-        <td class="inline-block  text-center w-52"></td>
-      </tr>
+      <th></th>
+    </tr>
 
-        <div  v-for="game in games" >
-      <tr class="border-2 border-blue-200">
-          <tr>
-            
-            <td class="inline-block w-52 text-center">{{ game.player }}</td>
-            <td class="inline-block w-52 text-center">{{ game.course }}</td>
-            <td class="inline-block w-52 text-center">{{ game.date }}</td>
-            <td class="inline-block w-52 text-center">{{ game.id }}</td>
-            
-            <td class="inline-block w-52 text-center"> <button @click="deleteGame(game.id)">DELETE</button></td>
-          
-          </tr>
-      </tr>
-        </div>
-    </table>
-    
-  </div>
+    <tr v-for="(game, id) in games" class="border-2 border-blue-200">
+      <td>{{ game.player }}</td>
+      <td>{{ game.course }}</td>
+      <td>{{ game.date }}</td>
+      <td>{{ game.id }}</td>
 
+      <td class="flex gap-2">
+        <button
+          class="
+            px-3
+            py-1
+            bg-slate-100
+            hover:bg-slate-300
+            rounded-xl
+            transition
+          "
+          @click="deleteGame(id)"
+        >
+          DELETE
+        </button>
+        <button
+          class="
+            px-3
+            py-1
+            bg-slate-100
+            hover:bg-slate-300
+            rounded-xl
+            transition
+          "
+          @click="editGame(id)"
+        >
+          Edit
+        </button>
+      </td>
+    </tr>
+  </table>
 </template>
